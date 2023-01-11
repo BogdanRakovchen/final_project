6e6a8a5ed0518e8313f6abc72782ab60e783b909
Bogdan Rakovchen <bogdan-dvzd.rzd@mail.ru>
2023-01-11 20:06:09 +1000

    add change

---
+++ src/main/java/pro/sky/final_project/FinalProjectApplication.java
@@ -0,0 +1,16 @@
+package pro.sky.final_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+
+@SpringBootApplication
+@OpenAPIDefinition
+public class FinalProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProjectApplication.class, args);
+	}
+
+}
